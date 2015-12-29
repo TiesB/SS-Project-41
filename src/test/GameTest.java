310e5620c8cf078274dc41558329009e495ab7d6
Ties Bolding <tiesbolding@outlook.com>
2015-12-30 00:36:20 +0100

    Update gitignore and git to not keep track of IDEA files.

---
+++ src/test/GameTest.java
@@ -0,0 +1,21 @@
+package test; /**
+ * Created by tiesb on 30-12-2015.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+    
+    @Before
+    public void setUp() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    
+}
